99155edee255138b067af1e93fb0d7153efc99da
Julien Noblet <julien.noblet@gmail.com>
2024-09-30 22:56:27 +0200

    Add asdf plugin for neovim

--- run_onchange_install-asdf.sh
+++ run_onchange_install-asdf.sh
@@ -6,9 +6,14 @@ if [ ! -d ~/.asdf ]; then
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
 fi
 
+# add asdf to shell
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
+
 asdf plugin add nodejs
 asdf plugin add age
 asdf plugin add github-cli
 asdf plugin add golang
 asdf plugin add lazygit
 asdf plugin add chezmoi
+asdf plugin add neovim

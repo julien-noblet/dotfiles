d3e9649fd0a3d67a26de94ff1e828f0ccebaf617
Julien Noblet <julien.noblet@gmail.com>
2024-09-30 22:12:44 +0200

    Add script to install asdf and plugins

--- run_onchange_install-asdf.sh
+++ run_onchange_install-asdf.sh
@@ -1,2 +1,14 @@
+#!/bin/sh
+
 # install asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+# if ~/.asdf not exist then install asdf
+if [ ! -d ~/.asdf ]; then
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+fi
+
+asdf plugin add nodejs
+asdf plugin add age
+asdf plugin add github-cli
+asdf plugin add golang
+asdf plugin add lazygit
+asdf plugin add chezmoi

8695ebc076298c826f090d0bd5acc17be120fff6
U-VICTUS\noble <julien.noblet@gmail.com>
2025-04-05 17:14:16 +0200

    Update home/.chezmoidata/winget.yaml Update home/.chezmoiscripts/winget.ps1 Update home/.config/powershell/Microsoft.PowerShell_profile.ps1 Add home/.config/powershell/Modules/WslInterop/0.4.1/LICENSE Add home/.config/powershell/Modules/WslInterop/0.4.1/PSGetModuleInfo.xml Add home/.config/powershell/Modules/WslInterop/0.4.1/README.md Add home/.config/powershell/Modules/WslInterop/0.4.1/WslInterop.psd1 Add home/.config/powershell/Modules/WslInterop/0.4.1/WslInterop.psm1 Add home/.config/powershell/Scripts/InstalledScriptInfos/.keep

--- home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,48 +1,72 @@
 
 # WSL Interop (https://github.com/mikebattista/PowerShell-WSL-Interop)
 # Installation: Install-Module WslInterop -Force
-Import-WslCommand "cat", "cp", "echo", "find", "grep", "head", "ls", "mv", "rm", "sed", "touch", "tree", "which"
+Import-WslCommand "cat","chmod", "cp", "echo", "find", "grep", "head", "less", "ls","man", "mv", "rm", "sed", "tail", "touch", "tree", "which"
 
 # WinGet (https://github.com/microsoft/winget-cli)
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
-      [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-      $Local:word = $wordToComplete.Replace('"', '""')
-      $Local:ast = $commandAst.ToString().Replace('"', '""')
-      winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-      }
+  param($wordToComplete, $commandAst, $cursorPosition)
+  [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+  $Local:word = $wordToComplete.Replace('"', '""')
+  $Local:ast = $commandAst.ToString().Replace('"', '""')
+  winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
   }
+}
+
+
+# Display if/which WSL Interop commands are imported.
+if ($WslImportedCommands) {
+  Write-Host "Windows Subsystem for Linux (WSL) Interop enabled." -ForegroundColor $ColorInfo
+  Write-Host "WSL commands available:`n`t$($WslImportedCommands | sort)" -ForegroundColor $ColorInfo
+}
   
-  #34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
-  if (Test-Path "C:\Program Files\PowerToys\WinGetCommandNotFound.psd1") {
-    Import-Module "C:\Program Files\PowerToys\WinGetCommandNotFound.psd1"
-  }
-  #34de4b3d-13a8-4540-b76d-b9e8d3851756
+#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
+if (Test-Path "C:\Program Files\PowerToys\WinGetCommandNotFound.psd1") {
+  Import-Module "C:\Program Files\PowerToys\WinGetCommandNotFound.psd1"
+}
+#34de4b3d-13a8-4540-b76d-b9e8d3851756
 # gsudo (https://github.com/gerardog/gsudo)
 # Installation: winget install gerardog.gsudo
 # PSWindowsUpdate (https://github.com/mgajda83/PSWindowsUpdate)
 # Installation: Install-Module PSWindowsUpdate -Force
 function Full-Upgrade {
-    gsudo {
-      if (Get-Command choco -ErrorAction SilentlyContinue) {
-        Write-Host '-> Upgrading Chocolatey packages'
-        choco upgrade all --yes
-      }
-  
-      Write-Host '-> Upgrading WinGet packages'
-      winget upgrade --all
-  
-      Write-Host '-> Triggering Microsoft Store updates'
-      # Source: https://social.technet.microsoft.com/Forums/windows/en-US/5ac7daa9-54e6-43c0-9746-293dcb8ef2ec
-      Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod > $null
-  
-      Write-Host '-> Installing Windows updates'
-      Get-WindowsUpdate -Install -AcceptAll
-  
-      Write-Host '-> Updating PowerShell modules'
-      Update-Module -Confirm:$false
+  gsudo {
+    if (Get-Command choco -ErrorAction SilentlyContinue) {
+      Write-Host '-> Upgrading Chocolatey packages'
+      choco upgrade all --yes
     }
+  
+    Write-Host '-> Upgrading WinGet packages'
+    winget upgrade --all
+  
+    Write-Host '-> Triggering Microsoft Store updates'
+    # Source: https://social.technet.microsoft.com/Forums/windows/en-US/5ac7daa9-54e6-43c0-9746-293dcb8ef2ec
+    Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod > $null
+  
+    Write-Host '-> Installing Windows updates'
+    Get-WindowsUpdate -Install -AcceptAll
+  
+    Write-Host '-> Updating PowerShell modules'
+    Update-Module -Confirm:$false
   }
+}
+
+$modules = (
+  #"posh-git",
+  #"oh-my-posh",
+  "Terminal-Icons",
+  "FastPing"
+)
+
+$modules | ForEach-Object {
+  if (Get-Module -ListAvailable -Name $_) {
+    Import-Module $_
+  }
+}
+if (Get-Module -ListAvailable -Name "Terminal-Icons") {
+  Set-TerminalIconsColorTheme -Name "DevBlackOps"
+}
+
 
 Invoke-Expression (&starship init powershell)

0b8bce5bcc359dc929ec216d520d747f220f4cab
Julien Noblet <julien.noblet@gmail.com>
2026-05-20 21:04:42 +0200

    Fix nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -27,8 +27,8 @@
   };
 
   # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  boot.loader.limine.enable = true;
+  boot.loader.systemd-boot.enable = true;
+  #boot.loader.limine.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   networking.hostName = "victus"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
